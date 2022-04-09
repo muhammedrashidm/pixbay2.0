6db0147595f40b5db5802711b9aa5ece3d68769d
muhammedrashidm <rashidotz@gmail.com>
2022-04-09 10:19:53 +0530

    Update api.dart

--- lib/services/api/api.dart
+++ lib/services/api/api.dart
@@ -4,7 +4,6 @@ class Api{
   final String apiKey;
 
   Api({required this.apiKey});
-  // 'https://pixabay.com/api/?key=25916027-470fe38377152e5d3cbfc7823'
 factory Api.production() =>Api(apiKey: ApiConstants.API_KEY);
 Uri initialUri(int page,String term)=> Uri.parse('https://pixabay.com/api/?key=${apiKey}&q=$term&page=$page');
  // Uri initialUri (String endpoint) => Uri(
